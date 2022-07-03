78291a50cabd189a12bef93c346bdbf5118a0355
Talha Junaid <talhajunaidd@gmail.com>
2022-07-03 21:36:47 +0500

    add readme and fix width of navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,10 +5,9 @@ import {Image} from "react-bootstrap";
 function NavBar() {
     return (
         <Navbar bg="light" expand="lg">
-
-            <Container>
+             <Container>
                 <div>
-                    <Image className="m-4" src={process.env.PUBLIC_URL + '/logo.png'}></Image>
+                    <Image className="m-4" width="10rem" src={process.env.PUBLIC_URL + '/logo.png'}></Image>
                     <Navbar.Brand href="#">SNS to SQS Message Converter</Navbar.Brand>
                 </div>
             </Container>
