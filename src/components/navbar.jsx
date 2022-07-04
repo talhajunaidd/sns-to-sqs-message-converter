5231cd695f55d46c4fc3a0a7aa06c27b2f851669
Talha Junaid <talhajunaidd@gmail.com>
2022-07-04 05:39:39 +0500

    add github link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,10 +5,11 @@ import {Image} from "react-bootstrap";
 function NavBar() {
     return (
         <Navbar bg="light" expand="lg">
-             <Container>
+            <Container>
                 <div>
                     <Image className="m-4" src={process.env.PUBLIC_URL + '/logo.png'}></Image>
                     <Navbar.Brand href="#">SNS to SQS Message Converter</Navbar.Brand>
+                    <a href="https://github.com/talhajunaidd/sns-to-sqs-message-converter">(Github)</a>
                 </div>
             </Container>
         </Navbar>
